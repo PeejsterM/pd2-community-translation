58196a6c0af3b86cbc1e5425db8354dc47b68ca1
PeejsterM <PeejsterM@users.noreply.github.com>
2015-06-09 19:14:39 -0500

    Update translation.lua
    Macros work now. Also, a lot of the script was changed.

--- translation.lua
+++ translation.lua
@@ -1,4 +1,3 @@
-local text_original = LocalizationManager.text -- Don't change this either.
 local printStrings = true -- This will output strings to text files inside the "translation" folder in your PAYDAY 2 directory.
 
 local useSillyTrans = true -- It's memeday, fellas!
@@ -20,39 +19,90 @@ Setting it to 2 will turn the gui into an absolute clusterfuck, so make sure you
 
 --the BLT hook doesn't handle localization changes in a way that allows us to use testAllStrings, so instead of using the BLT-friendly method, we are going to overwrite PAYDAY 2's localization function.
 function LocalizationManager:text(string_id, macros)
-	if noExportTable[string_id] == true then
-		pass_to_original(false, self, string_id, macros)
-		--I might do something with indices on the no export table that are set to values other than true, but right now I don't think it'll do anything useful.
+	local export = nil
+	local return_string = "ERROR: " .. string_id
+	local str_id = nil
+	local utsLocalizedThis = false
+	local bltLocalizedThis = false
+	
+	--This is just a multi-platform thing, I'm sure. Leave this here, though.
+
+	if( not string_id or string_id == "" ) then
+		return_string = ""
+	elseif( self:exists( string_id .. "_" .. self._platform ) ) then
+		str_id = string_id .. "_" .. self._platform
+	elseif( self:exists( string_id ) ) then
+		str_id = string_id
+	end
+	
+	--Check each of our translation tables.
+	
+	if utsTransTable[string_id] then
+		return_string = utsTransTable[string_id]
+		utsLocalizedThis = true
+	end
+	
+	if (useSillyTrans == true and sillyTransTable[string_id]) then
+		return_string = sillyTransTable[string_id]
+		utsLocalizedThis = true
+	end
+	if (useFixedTrans == true and fixedTransTable[string_id]) then
+		return_string = fixedTransTable[string_id]
+		utsLocalizedThis = true
+	end
+	if (useShortTrans == true and shortTransTable[string_id]) then
+		return_string = shortTransTable[string_id]
+		utsLocalizedThis = true
+	end
+	if (useReal_Trans == true and real_TransTable[string_id]) then
+		return_string = real_TransTable[string_id]
+		utsLocalizedThis = true
+	end
+	if (useGame_Trans == true and game_TransTable[string_id]) then
+		return_string = game_TransTable[string_id]
+		utsLocalizedThis = true
+	end
+	if (useOtherTrans == true and otherTransTable[string_id]) then
+		return_string = otherTransTable[string_id]
+		utsLocalizedThis = true
+	end
+	
+	--BLT Fallback.
+	
+	if self._custom_localizations[string_id] then
+		return_string = self._custom_localizations[string_id]
+		if macros and type(macros) == "table" then
+			bltLocalizedThis = true
+			for k, v in pairs( macros ) do
+				return_string = return_string:gsub( "$" .. k, v )
+			end
+		end
+	end
+	
+	--String hasn't been translated by BLT or by UTS, so let's export it if that option is available to us.
+	
+	if printStrings == true and not (utsLocalizedThis or bltLocalizedThis) and noExportTable[string_id] then
+		export = io.open("translation/" .. string_id .. ".txt", "w")
+		export:write(Localizer:lookup( Idstring(string_id)))
+	end
+	
+	if( str_id ) then
+		return_string_b = return_string
+		if utsLocalizedThis then
+			self._macro_context = macros
+			return_string = self:_localizer_post_process(return_string)
+			self._macro_context = nil
+		else
+			--string_print(str_id, return_string)
+			self._macro_context = macros
+			return_string = Localizer:lookup( Idstring( str_id ) )
+			self._macro_context = nil
+		end
+	end
+	
+	if export ~= nil then
+		export:close()
 	end
 
-	--unknown strings
-	if (useSillyTrans == true and sillyTransTable[string_id]) then return sillyTransTable[string_id] end
-	if (useFixedTrans == true and fixedTransTable[string_id]) then return fixedTransTable[string_id] end
-	if (useShortTrans == true and shortTransTable[string_id]) then return shortTransTable[string_id] end
-	if (useReal_Trans == true and real_TransTable[string_id]) then return real_TransTable[string_id] end
-	if (useGame_Trans == true and game_TransTable[string_id]) then return game_TransTable[string_id] end
-	if (useOtherTrans == true and otherTransTable[string_id]) then return otherTransTable[string_id] end
-	
-	--we don't yet have this string in any of our lists, so we're going to try testAllStrings
-	if (testAllStrings == 1) then return string_id end
-	if (testAllStrings == 2) then return string_id .. ": " .. pass_to_original(true, self, string_id, macros) end
-	
-	--Fallback.
-	return pass_to_original(true, self, string_id, macros)
-end
-
-function pass_to_original(writeThis, self, string_id, macros)
-	if printStrings == true and writeThis then
-		local _parseMacros = parseMacros
-		parseMacros = writeMacros
-		string_print(string_id, text_original(self, string_id, macros))
-		parseMacros = _parseMacros
-	end
-	return text_original(self, string_id, macros)
-end
-
-function string_print(string_id, string_original, macros)
-	local export = io.open("translation/" .. string_id .. ".txt", "w")
-	export:write(string_original)
-	export:close()
+	return return_string
 end
